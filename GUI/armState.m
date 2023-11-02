b0cdd1d9d9a4efe8d327a92fb7f43c9c43a0717e
AlbertIsoi <126046756+AlbertIsoi@users.noreply.github.com>
2023-11-02 20:15:30 +1100

    Add files via upload

---
+++ GUI/armState.m
@@ -0,0 +1,16 @@
+classdef armState
+    enumeration
+        Init
+        AssignedNextTr
+        MovingToNextTr
+        ArrivedAtNextTr
+        WaitingForNextTr
+        EStop
+
+        Operating
+        Stopped
+        EStopped
+        ReadyToResume
+
+    end %Arm states
+end
